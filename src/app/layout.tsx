bf58d48a7b45ffc17d139913a37bcd9acb9cdc7c
rajeev singh <singhrajeev1470@gmail.com>
2025-12-02 14:15:33 +0000

    forthis website this the result of google insight why performence is ve

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,11 +26,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3080938150148610"
-          crossOrigin="anonymous"
-        ></script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
